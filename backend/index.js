a222058dd17f98dc9b2181a3e0a014f81d6883bd
The winner <akkhilsharmaclass@gmail.com>
2023-09-02 09:24:39 +0530

    git push

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,25 @@
 // Function to fetch and display books from the backend
 async function fetchBooks() {
     try {
-        const response = await fetch('/books'); // Assumes your backend server is running on the same domain
+        const response = await fetch('/api/books'); // Endpoint should match your server route
         const books = await response.json();
         
         // Display the books on your webpage
-        const bookList = document.getElementById("bookList");
-
-        books.forEach(book => {
-            const li = document.createElement("li");
-            li.textContent = `${book.title} by ${book.author} (${book.publicationYear}) - ISBN: ${book.isbn}`;
-            bookList.appendChild(li);
-        });
+        bookDisplay(books);
     } catch (error) {
         console.error("Error fetching books:", error);
     }
+}
+
+// Function to display books on the webpage
+function bookDisplay(books) {
+    const bookList = document.getElementById("bookList");
+
+    books.forEach(book => {
+        const li = document.createElement("li");
+        li.textContent = `${book.title} by ${book.author} (${book.publicationYear}) - ISBN: ${book.isbn}`;
+        bookList.appendChild(li);
+    });
 }
 
 // Call the fetchBooks function when the page loads
